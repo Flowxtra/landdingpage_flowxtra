4f95c4fd348c53b2bccc152091d40e5b9a9feb95
Mohamed Alarade <45152485+9mtm@users.noreply.github.com>
2025-11-19 13:51:19 +0100

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Flowxtra- Free recruiting software & applicant tracking system",
   description:
-    "Flowxtra is a free recruiting software and Applicant Tracking System (ATS) that helps companies post jobs for free, use social media recruiting, and efficiently manage applicants with AI-powered hiring tools.
+    "Flowxtra is a free recruiting software and Applicant Tracking System (ATS) that helps companies post jobs for free, use social media recruiting, and efficiently manage applicants with AI-powered hiring tools.",
 };
 
 export default function RootLayout({
