1004b9873384fc89b8a4eef45b904d50495ed4f5
Mohamed Alarade <45152485+9mtm@users.noreply.github.com>
2025-11-19 14:28:20 +0100

    fix roborts file

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,46 @@ export default function robots(): MetadataRoute.Robots {
   // Base URL from environment variable or default to production
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://flowxtra.com";
 
+  const sitemapPaths = [
+    "/sitemap.xml",
+    "/sitemap_index.xml",
+    "/sitemap-all.xml",
+    "/sitemap-ar.xml",
+    "/sitemap-static-ar.xml",
+    "/sitemap-ar-app-store-0.xml",
+    "/sitemap-de.xml",
+    "/sitemap-static-de.xml",
+    "/sitemap-de-app-store-0.xml",
+    "/sitemap-de-at.xml",
+    "/sitemap-static-de-at.xml",
+    "/sitemap-de-ch.xml",
+    "/sitemap-static-de-ch.xml",
+    "/sitemap-en.xml",
+    "/sitemap-static-en.xml",
+    "/sitemap-en-blog-0.xml",
+    "/sitemap-en-app-store-0.xml",
+    "/sitemap-en-au.xml",
+    "/sitemap-static-en-au.xml",
+    "/sitemap-en-ca.xml",
+    "/sitemap-static-en-ca.xml",
+    "/sitemap-en-gb.xml",
+    "/sitemap-static-en-gb.xml",
+    "/sitemap-en-us.xml",
+    "/sitemap-static-en-us.xml",
+    "/sitemap-es.xml",
+    "/sitemap-static-es.xml",
+    "/sitemap-es-app-store-0.xml",
+    "/sitemap-fr.xml",
+    "/sitemap-static-fr.xml",
+    "/sitemap-fr-app-store-0.xml",
+    "/sitemap-it.xml",
+    "/sitemap-static-it.xml",
+    "/sitemap-it-app-store-0.xml",
+    "/sitemap-nl.xml",
+    "/sitemap-static-nl.xml",
+    "/sitemap-nl-app-store-0.xml",
+  ];
+
   return {
     rules: [
       {
@@ -35,7 +75,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/", "/_next/", "/admin/", "/private/"],
       },
     ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    sitemap: sitemapPaths.map((path) => `${baseUrl}${path}`),
     host: baseUrl,
   };
 }
